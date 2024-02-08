5392597df4907ad01ddaec41156caae5bf4d1bf8
dhruv <dhruvsharma0505@gmail.com>
2024-02-08 11:57:36 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-black text-white font-bold text-center ">
+      namaste doston, lets build netflix gpt!
+    </div>
+  );
+}
+
+export default App;
